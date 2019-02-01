e3b92cd87820a2ef61cfd01e2536708fe591e1a0
Urszula Fratczak <urszula.fratczakk@gmail.com>
2019-02-01 18:39:41 +0100

    initial commit

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Dashboard.css';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="dashboard">
+        Dashboard
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
